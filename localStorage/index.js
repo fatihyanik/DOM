3360498e71adc4217801ab2a727efc7a761cbe50
fatihyanik <fatihyanik07@gmail.com>
2021-11-10 20:16:18 +0100

    I added localStorage

--- localStorage/index.js
+++ localStorage/index.js
@@ -1,4 +1,4 @@
-const users = {
+/* const users = {
     "firstName" :"Murat",
     "lastName" :"Yanik"
 }
@@ -10,4 +10,37 @@ localStorage.setItem("users", JSON.stringify(users));
 
 const newUser = JSON.parse(localStorage.getItem("users"));
 console.log(newUser);
-console.log(newUser["lastName"]);
+console.log(newUser["lastName"]); */
+
+const ulList = document.querySelector('.list');
+const nameArr = JSON.parse(localStorage.getItem('nameList'));
+
+if (nameArr != null) {
+    nameArr.forEach(function (name) {
+        const newList = document.createElement('li');
+        newList.textContent = name;
+        ulList.appendChild(newList);
+    }); 
+}
+
+
+document.querySelector('#form').addEventListener('submit', function (e) {
+    const newName = document.querySelector('.name').value;
+    let nameArr;
+
+    if (localStorage.getItem('nameList') === null) {
+        nameArr = [];
+    } else {
+        nameArr = JSON.parse(localStorage.getItem('nameList'))
+    }
+
+    nameArr.push(newName);
+
+    localStorage.setItem('nameList', JSON.stringify(nameArr));
+    const newList = document.createElement('li');
+    newList.textContent = newName;
+    ulList.appendChild(newList);
+
+
+    e.preventDefault();
+});
